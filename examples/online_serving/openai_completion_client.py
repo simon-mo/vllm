5e43e1c32da82b2f87554b90affb94301e94f920
simon-mo <simon.mo@hey.com>
2025-07-24 07:36:15 +0000

    restore extranous file

--- examples/online_serving/openai_completion_client.py
+++ examples/online_serving/openai_completion_client.py
@@ -1,60 +1,53 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-import uuid
+import argparse
 
 from openai import OpenAI
 
-# 1: Set up your clients
+# Modify OpenAI's API key and API base to use vLLM's API server.
 openai_api_key = "EMPTY"
-prefill_client = OpenAI(
-    api_key=openai_api_key,
-    base_url="http://IP_ADDRESS_OF_PREFILL_INSTANCE:8000/v1",
-)
-decode_client = OpenAI(
-    api_key=openai_api_key,
-    base_url="http://IP_ADDRESS_OF_DECODE_INSTANCE:8001/v1",
-)
-models = prefill_client.models.list()
-model = models.data[0].id
-
-# 2: Prefill
-request_id = str(
-    uuid.uuid4()
-)  # maintain the same request_id for both prefill and decode
-response = prefill_client.completions.create(
-    model=model,
-    # PD only works when the prompt is greater than vLLM's block size (16 tokens)
-    prompt="A long prompt greater than 16 tokens",
-    # Set this to force only prefill
-    max_tokens=1,
-    # This will be echoed back to pass to decode
-    extra_body={
-        "kv_transfer_params": {
-            "do_remote_decode": True,
-            "do_remote_prefill": False,
-            "remote_engine_id": None,
-            "remote_block_ids": None,
-            "remote_host": None,
-            "remote_port": None,
-        }
-    },
-    extra_headers={"X-Request-Id": request_id},
-)
-print("-" * 50)
-print("Prefill results:")
-print(response)
+openai_api_base = "http://localhost:8000/v1"
 
 
-# 3: Decode
-# Pass the kv_transfer_params from prefill to decode
-decode_response = decode_client.completions.create(
-    model=model,
-    prompt="A robot may not injure a human being",
-    extra_body={"kv_transfer_params": response.kv_transfer_params},
-    extra_headers={"X-Request-Id": request_id},
-)
-print("-" * 50)
-print("Decode results:")
-print(decode_response)
-print("-" * 50)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Client for vLLM API server")
+    parser.add_argument(
+        "--stream", action="store_true", help="Enable streaming response"
+    )
+    return parser.parse_args()
+
+
+def main(args):
+    client = OpenAI(
+        # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key=openai_api_key,
+        base_url=openai_api_base,
+    )
+
+    models = client.models.list()
+    model = models.data[0].id
+
+    # Completion API
+    completion = client.completions.create(
+        model=model,
+        prompt="A robot may not injure a human being",
+        echo=False,
+        n=2,
+        stream=args.stream,
+        logprobs=3,
+    )
+
+    print("-" * 50)
+    print("Completion results:")
+    if args.stream:
+        for c in completion:
+            print(c)
+    else:
+        print(completion)
+    print("-" * 50)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
